c514aed0b1438396c43a23b3c859605f3effcc65
jcarrus <carrus.justin@gmail.com>
2014-08-06 21:50:40 -0700

    First version of bubbles

--- visualization/js/visualization.js
+++ visualization/js/visualization.js
@@ -1,24 +1,27 @@
 var color = d3.scale.quantize()
     .range(["#156b87", "#876315", "#543510", "#872815"]);
 
-var size = 960, padding = 5;
+var size = 960;
 
 var pack = d3.layout.pack()
-    .sort(null)
+    .sort(function(a, b){ return a.separation - b.separation;})
     .size([size, size])
     .value(function(d) { return d.importance; })
-    .padding(padding);
+    .padding(5);
 
 var svg = d3.select("body").append("svg")
     .attr("width", size)
     .attr("height", size);
 
-d3.csv("http://web.mit.edu/jcarrus/Public/links.csv", undefined, function(error, links){
-    console.log(links);
-    console.log(pack.nodes());
+color.domain(d3.extent(links, function(d) { return d.separation; }));
 
-    color.domain(d3.extent(exoplanets, function(d) { return d.distance; }));
-});
-    // svg.selectAll("circle")
-    // 	.data(pack.nodes({c
-	
+svg.selectAll("circle")
+      .data(pack.nodes({children: links}))
+    .enter().append("circle")
+      .attr( "r",    function(d){ return d.importance;        })
+      .attr( "cx",   function(d){ return d.x;                 })
+      .attr( "cy",   function(d){ return d.y;                 })
+      .style("fill", function(d){ return color(d.separation); })
+    .append("title")
+      .text(function(d){ return d.name;});
+d3.select(self.frameElement).style("height", size + "px");
