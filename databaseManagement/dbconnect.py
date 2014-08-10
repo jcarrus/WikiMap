e09a82334a3880e07a8a13804fde37e61ccc645d
jcarrus <carrus.justin@gmail.com>
2014-08-09 19:33:28 -0700

    Initial table upload

--- databaseManagement/dbconnect.py
+++ databaseManagement/dbconnect.py
@@ -1,6 +1,24 @@
+import os
 import MySQLdb as mdb
+import csv
 
-def connect():
+def loadCSV():
     con = mdb.connect('sql.mit.edu', 'jcarrus', 'ver17map',
                       'jcarrus+WikiMap')
-    return con.cursor()
+    cur = con.cursor()
+    index = 0
+    cur.execute("TRUNCATE TABLE ids")
+    with open('WikiMapAll', 'rb') as file:
+        csvreader = csv.reader(file)
+        for i in csvreader:
+            index += 1
+            cur.execute("""
+            INSERT INTO ids
+            (curid, title)
+            VALUES (%s, %s);
+            """, (i[0],i[1]))
+            if index % 100 == 0:
+                print index
+
+
+loadCSV()
