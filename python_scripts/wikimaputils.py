c70c9d194213ffc82e200fc50344ae2f3da72a2f
jcarrus <carrus.justin@gmail.com>
2014-08-13 10:53:00 -0700

    Add queue support for file write

--- python_scripts/wikimaputils.py
+++ python_scripts/wikimaputils.py
@@ -1,5 +1,7 @@
 import os
 import datetime
+from Queue import Queue
+from threading import Thread
 
 class Record:
 
@@ -7,23 +9,35 @@ class Record:
         self.directory = directory
         self.numLines = numLines
         self.now = str(datetime.datetime.now())
-        self.currentLines = 0
-        self.filenum = 1
 
-        if not os.path.exists('../data/%s/%s/' % (self.directory, self.now)):
-            os.makedirs('../data/%s/%s/' % (self.directory, self.now))
+        self.q = Queue(max_queue_size)
+        self.t = Thread(target = writeQueue, args = (self.directory, self.now, self.filenum, q, self.numlines))
+        self.t.daemon = true
+        self.t.start()
 
-        self.f = open('../data/%s/%s/%05d' % (self.directory, self.now, self.filenum), 'w')
-        
+
+    # Need to figure out how to stop this properly. I could have the close method just
+    # send a line that would tell the queue to close. But that seems a bit messy.
+    def writeQueue(starttime, directory, q, numlines):
+        if not os.path.exists('../data/%s/%s/' % (directory, starttime)):
+            os.makedirs('../data/%s/%s/' % (directory, starttime))
+        filenum = 1
+        f = open('../data/%s/%s/%05d' % (directory, starttime, filenum), 'w')
+        currentLines = 0
+        while True:
+            line = q.get(True)
+            try:
+                f.write(line + '\n')
+                currentLines += 1
+                if (currentLines % numLines == 0):
+                    f.close()
+                    currentLines = 0
+                    filenum += 1
+                    f = open('../data/%s/%s/%05d' % (directory, now, filenum), 'w')
+            except:
+                print "write error on line: %s" % line
 
     def writeline(self, line):
-        self.f.write(line + '\n')
-        self.currentLines += 1
-        if (self.currentLines % self.numLines == 0):
-            self.f.close()
-            self.currentLines = 0
-            self.filenum += 1
-            self.f = open('../data/%s/%s/%05d' % (self.directory, self.now, self.filenum), 'w')
-    
+        self.q.put(line, True)
     def close(self):
         self.f.close()
