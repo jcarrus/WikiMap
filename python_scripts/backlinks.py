ff2d19f201ef5c06ebf5337e0c003c095b13703e
jcarrus <carrus.justin@gmail.com>
2014-08-13 18:25:18 -0700

    Add multithreading to backlinks parse

--- python_scripts/backlinks.py
+++ python_scripts/backlinks.py
@@ -1,30 +1,62 @@
 import requests
 import csv
+from threading import Thread
 from wikimaputils import Record
+from Queue import Queue
 
 def getAllBacklinks():
+    numThreads = 25
     writer = Record('backlinks', 10000)
-    links = csv.reader(open('WikiMapAll', 'rb'))
     errors = Record('errors', 10000)
+    linksQueue = Queue(numThreads * 2)
+
+    # Make a bunch of threads
+    for i in range(numThreads):
+        t = Thread(target = backlinksWorker, args = (linksQueue, writer))
+        t.daemon = True
+        t.start()
+
+    # Read from the csv and add each link to the queue
+    links = csv.reader(open('WikiMapAll', 'rb'))
     for link in links:
-        try:
-            backlinks = getBacklinksForId(link[0])
-            for backlink in backlinks:
-                writer.writeline(str(link[0]) + ',' + str(backlink))
-        except:
-            errors.writeline(str(link))
+        linksQueue.put(link[0], True)
+
+    # Wait for the queue to finish
+    linksQueue.join()
+
+    # Close the files
     writer.close()
     errors.close()
 
+
+def backlinksWorker(q, r):
+    while True:
+        try:
+            # Pull from the queue
+            frontlink = q.get(True)
+            # Get all backlinks
+            links = getBacklinksForId(frontlink)
+            # Write to the file
+            for link in links:
+                r.writeline(str(frontlink) + ',' + str(link))
+            # Report finished
+            q.task_done()
+        except:
+            errors.writeline(str(frontlink))
+            
 def getBacklinksForId(page_id, blcontinue = ''):
     results = []
+    # Query the database
     result = requests.get(getQuery(page_id, blcontinue)).json()
+    # Add all of the backlinks to the results
     for i in result['query']['backlinks']:
         results.append(i['pageid'])
+    # If there are no more, return, else, repeat.
     if 'query-continue' not in result:
         return results
     return results + getBacklinksForId(page_id, result['query-continue']['backlinks']['blcontinue'])
 
+# Creates the apropriate Wikipedia query for given parameters
 def getQuery(page_id, mycontinue = "", list = "backlinks", num_results = 500):
     mystr = 'http://en.wikipedia.org/w/api.php?' \
         + 'format=json' \
@@ -37,7 +69,8 @@ def getQuery(page_id, mycontinue = "", list = "backlinks", num_results = 500):
         mystr += '&blcontinue=' + url_fix(mycontinue)
     return mystr
 
-def url_fix(s):
-    return s.replace("&", "%26")
+# Removes undesired characters from the created query url
+def url_fix(url):
+    return url.replace("&", "%26")
 
 getAllBacklinks()
--- python_scripts/wikimaputils.py
+++ python_scripts/wikimaputils.py
@@ -2,42 +2,48 @@ import os
 import datetime
 from Queue import Queue
 from threading import Thread
-
+import time
 class Record:
-
-    def __init__(self, directory, numLines):
+    
+    def __init__(self, directory, numLines, max_queue_size = 400):
         self.directory = directory
         self.numLines = numLines
-        self.now = str(datetime.datetime.now())
+        self.now = time.time() #str(datetime.datetime.now())
 
         self.q = Queue(max_queue_size)
-        self.t = Thread(target = writeQueue, args = (self.directory, self.now, self.filenum, q, self.numlines))
-        self.t.daemon = true
+        self.t = Thread(target = writeQueue, args = (self.now, self.directory, self.q, self.numLines))
+        self.t.daemon = True
         self.t.start()
-
-
-    # Need to figure out how to stop this properly. I could have the close method just
-    # send a line that would tell the queue to close. But that seems a bit messy.
-    def writeQueue(starttime, directory, q, numlines):
-        if not os.path.exists('../data/%s/%s/' % (directory, starttime)):
-            os.makedirs('../data/%s/%s/' % (directory, starttime))
-        filenum = 1
-        f = open('../data/%s/%s/%05d' % (directory, starttime, filenum), 'w')
-        currentLines = 0
-        while True:
-            line = q.get(True)
-            try:
-                f.write(line + '\n')
-                currentLines += 1
-                if (currentLines % numLines == 0):
-                    f.close()
-                    currentLines = 0
-                    filenum += 1
-                    f = open('../data/%s/%s/%05d' % (directory, now, filenum), 'w')
-            except:
-                print "write error on line: %s" % line
 
     def writeline(self, line):
         self.q.put(line, True)
+
     def close(self):
-        self.f.close()
+        self.q.put(False, True)
+
+def writeQueue(starttime, directory, q, numLines):
+    if not os.path.exists('../data/%s/%s/' % (directory, starttime)):
+        os.makedirs('../data/%s/%s/' % (directory, starttime))
+    filenum = 1
+    f = open('../data/%s/%s/%05d' % (directory, starttime, filenum), 'w')
+    currentLines = 0
+    while True:
+        line = q.get(True)
+        if line == False:
+            q.join()
+            f.close()
+            return
+        try:
+            f.write(str(line) + '\n')
+            currentLines += 1
+            if (currentLines % numLines == 0):
+                f.close()
+                currentLines = 0
+                filenum += 1
+                print time.time() - starttime
+                return
+                f = open('../data/%s/%s/%05d' % (directory, now, filenum), 'w')
+            q.task_done()
+        except:
+            print "write error on line: %s" % line
+            raise
